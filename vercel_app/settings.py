5f199df11707fc2ea958e4bd14fb5b49c8ed59bc
bacdor <bachlaj.dorian@gmail.com>
2024-06-20 16:03:11 -0400

    database

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -66,14 +66,14 @@ WSGI_APPLICATION = 'vercel_app.wsgi.app'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
-    #     "NAME": os.environ.get("DB_NAME"),
-    #     "USER": os.environ.get("DB_USER"),
-    #     "PASSWORD": os.environ.get("DB_PASSWORD"),
-    #     "HOST": os.environ.get("DB_HOST"),
-    #     "PORT": os.environ.get("DB_PORT"),
-    # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+    }
 }
 
 # Password validation
